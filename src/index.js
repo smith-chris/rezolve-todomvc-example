9a19cf818901b49d9f62494142d03bf27f4839b5
smith-chris <kowalski.k@outlook.com>
2019-06-25 16:21:27 +0200

    Add support for TS and redux devtools.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import reducer from './reducers'
 import 'todomvc-app-css/index.css'
-
-const store = createStore(reducer)
+import { store } from './store'
 
 render(
   <Provider store={store}>
